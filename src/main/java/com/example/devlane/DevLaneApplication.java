1ebf36da51e6809f8a97cd16a96ce8b0c17e648e
Anurag Manu <anuragmanu2311@gmail.com>
2023-05-18 22:44:25 +0530

    Initial commit for DevLane

---
+++ src/main/java/com/example/devlane/DevLaneApplication.java
@@ -0,0 +1,13 @@
+package com.example.devlane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevLaneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devlane/DevLaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devlane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevLaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
